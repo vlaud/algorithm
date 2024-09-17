694d38ad24e2a4230a67beabdc8c819d7325840b
vlaud <whehdgus1207@naver.com>
2024-09-17 15:13:19 +0900

    버블 정렬

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,88 +1,87 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
 #include <vector>
-#include <queue>
-#include <stack>
 #include <algorithm>
-#include <cmath>
-#define endl "\n"
+#include <sstream>
+#include <unordered_map>
+
 using namespace std;
 
-bool dVisited[1001];
-bool bVisited[1001];
-vector<vector<int>> graph(1001);
-int from[1001];
-stack<int> track;
+#define endl "\n"
 
-void bfs(int source, int destination) {
-	queue<int> q;
-	q.push(source);
-	bVisited[source] = true;
-	from[source] = 0;
-	int current = 0;
-
-	while (!q.empty()) {
-		current = q.front();
-		cout << current << " ";
-		q.pop();
-
-		for (int next : graph[current]) {
-			if (!bVisited[next]) {
-				q.push(next);
-				bVisited[next] = true;
-				from[next] = current;
-			}
-		}
-	}
-	cout << "final: " << current << endl;
-	cout << endl;
-	cout << endl;
-	current = destination;
-
-	while (current != 0) {
-		cout << current << " is from: " << from[current] << endl;
-		track.push(current);
-		current = from[current];
-	}
-	while (!track.empty()) {
-		cout << track.top() << " ";
-		track.pop();
-	}
-}
-void dfs(int current) {
-	
-	dVisited[current] = true;
-	cout << current << " ";
-
-	for (int next : graph[current]) {
-		if (!dVisited[next]) {
-			dfs(next);
-		}
-	}
+double getRadian(double degree) {
+    return degree * M_PI / 180;
 }
 
+double getDestination(vector<double> a, vector<double> b) {
+    double x = (b[0] - a[0]) * cos((a[1] + b[1]) * 0.5f);
+    double y = b[1] - a[1];
+
+    double d = sqrt(pow(x, 2) + pow(y, 2)) * 6371;
+
+    return d;
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int N, M, V, D;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    /*string pullPath = "1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217";
+    stringstream s_longitude;
+    stringstream s_latitude;
+    
+    long double longitude, latitude;
+    double rLong, rLati;
 
-	cin >> N >> M >> V >> D;
-	
-	int a, b;
-	for (int i = 0; i < M; i++) {
-		cin >> a >> b;
-		graph[a].push_back(b);
-		graph[b].push_back(a);
-	}
-	
-	for (int i = 0; i <= N; i++) {
-		sort(graph[i].begin(), graph[i].end());
-	}
+    replace(pullPath.begin(), pullPath.end(), ',', '.');
 
-	bfs(V, D);
-	cout << endl;
-	
-	return 0;
+    int twoSemi = pullPath.rfind(";;") + 2;
+    int oneSemi = pullPath.rfind(";") + 1;
+
+    s_longitude.str(pullPath.substr(twoSemi, oneSemi - twoSemi - 1));
+    s_longitude >> longitude;
+    s_latitude.str(pullPath.substr(oneSemi));
+    s_latitude >> latitude;
+    rLong = getRadian(longitude);
+    rLati = getRadian(latitude);
+
+    cout.precision(30);
+    cout << pullPath.substr(twoSemi, oneSemi - twoSemi - 1) << endl;
+    cout << longitude << endl;
+    cout << rLong << endl;
+    
+    cout << pullPath.substr(oneSemi) << endl;
+    cout << latitude << endl;
+    cout << rLati << endl;*/
+
+    int nums[5] = { 7, 5, 6, 3, 1 };
+
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    int temp = 0;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                temp = nums[j + 1];
+                nums[j + 1] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        for (int num : nums) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
 }
