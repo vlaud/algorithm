627daa4b23d46c1cc61cf2ed2fb9e83e1522407e
vlaud <whehdgus1207@naver.com>
2025-10-30 21:31:31 +0900

    [실전 알고리즘] 0x05강 - 스택

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -6,83 +6,33 @@
 #define endl '\n'
 using namespace std;
 
-const int mx = 1000005;
-char dat[mx];
-int prv[mx], nxt[mx];
-int unused = 1; //tail = 0;
+const int MX = 1000005;
+int dat[MX];
+int pos = 0;
 
-void insert(int adrs, char val) {
-	dat[unused] = val;
-	prv[unused] = adrs;
-	nxt[unused] = nxt[adrs];
-
-	if (nxt[adrs] != -1) prv[nxt[adrs]] = unused;
-	//else tail = unused;
-
-	nxt[adrs] = unused;
-
-	unused++;
+void push(int x) {
+	dat[pos++] = x;
 }
 
-void erase(int adrs) {
-	nxt[prv[adrs]] = nxt[adrs];
-
-	if (nxt[adrs] != -1) prv[nxt[adrs]] = prv[adrs];
-	//else tail = prv[adrs];
-
-
-	//unused--;
+void pop() {
+	pos--;
 }
 
-void priting() {
-	int cur = nxt[0];
-	while (cur != -1) {
-		cout << dat[cur];
-		cur = nxt[cur];
-	}
-	cout << endl;
+int top() {
+	return dat[pos-1];
 }
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+void test() {
+	push(5); push(4); push(3);
+	cout << top() << '\n'; // 3
+	pop(); pop();
+	cout << top() << '\n'; // 5
+	push(10); push(12);
+	cout << top() << '\n'; // 12
+	pop();
+	cout << top() << '\n'; // 10
+}
 
-	fill(prv, prv + mx, -1);
-	fill(nxt, nxt + mx, -1);
-	string s;
-	cin >> s;
-	int p = 0;
-
-	for (char c : s) {
-		insert(p, c);
-		p++;
-	}
-
-	int n = 0;
-	char input;
-	
-	cin >> n;
-	while (n--) {
-		cin >> input;
-
-		if (input == 'P') {
-			cin >> input;
-			insert(p, input);
-			p = nxt[p];
-		}
-		else if (input == 'L') {
-			if (prv[p] != -1) p = prv[p];
-		}
-		else if (input == 'D') {
-			if (nxt[p] != -1) p = nxt[p];
-		}
-		else {
-			if (prv[p] != -1) {
-				erase(p);
-				p = prv[p];
-			}
-		}
-	}
-	priting();
-	return 0;
+int main(void) {
+	test();
 }
