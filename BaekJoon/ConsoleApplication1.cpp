e884721c4b606c14ffcaef6cd5beb0e07d16d394
vlaud <whehdgus1207@naver.com>
2025-11-12 16:19:26 +0900

    백준 2141번: 우체국 - 정렬 + 그리디

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using ll = long long;
 using iPair = pair<int, int>;
 
-iPair xa[static_cast<int>(1e5)];
+iPair xa[100000];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -21,25 +21,18 @@ int main() {
 	
 	for (int i = 0; i<n; i++){
 		cin >> xa[i].first >> xa[i].second;
-		xa[i].first += 1e9;
+		sum += xa[i].second;
 	}
 
-	ll s, l = 0, r = 2e9;
-	ll mid = l + (r - l) / 2;
+	sort(xa, xa + n);
 
-	while (l < r) {
-		s = 0;
-		for (int i = 0; i < n; i++) {
-			s += (abs(xa[i].first - mid - 1) - abs(xa[i].first - mid)) * xa[i].second;
+	for (int i = 0; i < n; i++) {
+		sum -= 2 * xa[i].second;
+		if (sum <= 0) {
+			cout << xa[i].first;
+			break;
 		}
-		if (s >= 0) r = mid;
-		else l = mid + 1;
-
-		mid = l + (r - l) / 2;
 	}
-
-	mid -= 1e9;
-	cout << mid;
 
 	return 0;
 }
