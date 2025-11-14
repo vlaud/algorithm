684d915ba5472b00d719ac1f0c6a89319dc4dc0d
vlaud <whehdgus1207@naver.com>
2025-11-14 16:08:04 +0900

    [실전 알고리즘] 0x07강 - 덱

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,38 +1,51 @@
 ﻿#include <iostream>
-#include <algorithm>
 
 #define endl '\n'
 
 using namespace std;
-using ll = long long;
-using iPair = pair<int, int>;
 
-iPair xa[100000];
+const int mx = 1000005;
+int dat[2 * mx + 1];
+int head = mx, tail = mx;
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+void push_front(int x) {
+	dat[--head] = x;
+}
 
-	int n, idx = 0;
-	ll sum = 0;
-	
-	cin >> n;
-	
-	for (int i = 0; i<n; i++){
-		cin >> xa[i].first >> xa[i].second;
-		sum += xa[i].second;
-	}
+void push_back(int x) {
+	dat[tail++] = x;
+}
 
-	sort(xa, xa + n);
+void pop_front() {
+	head++;
+}
 
-	for (int i = 0; i < n; i++) {
-		sum -= 2 * xa[i].second;
-		if (sum <= 0) {
-			cout << xa[i].first;
-			break;
-		}
-	}
+void pop_back() {
+	tail--;
+}
 
-	return 0;
+int front() {
+	return dat[head];
+}
+
+int back() {
+	return dat[tail-1];
+}
+
+void test() {
+	push_back(30); // 30
+	cout << front() << '\n'; // 30
+	cout << back() << '\n'; // 30
+	push_front(25); // 25 30
+	push_back(12); // 25 30 12
+	cout << back() << '\n'; // 12
+	push_back(62); // 25 30 12 62
+	pop_front(); // 30 12 62
+	cout << front() << '\n'; // 30
+	pop_front(); // 12 62
+	cout << back() << '\n'; // 62
+}
+
+int main(void) {
+	test();
 }
