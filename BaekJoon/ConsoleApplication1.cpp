d116ee59f6c788d005e9ca4648faaf3262a4b7fc
vlaud <whehdgus1207@naver.com>
2025-11-28 18:37:08 +0900

    힙 구현

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,67 +1,80 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <string>
-#include <typeinfo>
-
 using namespace std;
 
 #define endl '\n'
+#define mx 1000005
 
-int main()
-{
-	int arr[2][3] = { {1,2,3},
-					  {4,5,6}, };
+template<typename T>
+class Heap {
+private:
+	T* arr;
+	int size = 0;
+public:
+	Heap();
+	~Heap();
+	void push(T val);
+	T top();
+	int getSize();
+	void pop();
+};
+template<typename T>
+Heap<T>::Heap() {
+	arr = new T[mx];
+}
+template<typename T>
+Heap<T>::~Heap() {
+	delete[] arr;
+	arr = nullptr;
+}
+template<typename T>
+void Heap<T>::push(T val) {
+	int i = ++size;
 
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			printf("%d ", arr[i][j]);
-		}
-		cout << endl;
+	// 최소 힙
+	while (i != 1 && arr[i / 2] > val) {
+		arr[i] = arr[i / 2];
+		i /= 2;
 	}
-	int (*pt)[3] = arr;
-	int att[2] = { 1,3 };
-	int* p = att;
-	int* pthh[3] = { new int[2]{1,5}, new int[3]{4,5,5}, new int[2]{2,6} };
-	cout << endl;
+	arr[i] = val;
+}
+template<typename T>
+T Heap<T>::top() {
+	return arr[1];
+}
+template<typename T>
+int Heap<T>::getSize() {
+	return size;
+}
+template<typename T>
+void Heap<T>::pop() {
+	if (!size) return;
 
-	printf("pt[0][2] = %d\n", pt[0][2]);
-	printf("typeid(arr) = %s\n", typeid(arr).name());
-	printf("typeid(&arr[0]) = %s\n", typeid(&arr[0]).name());
-	printf("typeid(*arr) = %s\n", typeid(*arr).name());
-	printf("typeid(arr[0]) = %s\n", typeid(arr[0]).name());
-	printf("typeid(&arr[0][0]) = %s\n", typeid(&arr[0][0]).name());
-	printf("typeid(**arr) = %s\n", typeid(**arr).name());
-	printf("typeid(*arr[0]) = %s\n", typeid(*arr[0]).name());
-	printf("typeid(att) = %s\n", typeid(att).name());
-	printf("typeid(p) = %s\n", typeid(p).name());
-	printf("typeid(pthh) = %s\n", typeid(pthh).name());
-	printf("typeid(pthh[0][1]) = %s\n", typeid(pthh[0][1]).name());
-	printf("typeid(&pthh[0][1]) = %s\n", typeid(&pthh[0][1]).name());
-	printf("pthh[0][1] = %d\n", pthh[0][1]);
+	T temp = arr[size--];
+	int p = 1, c = 2;
 
-	cout << endl;
-	cout << "[] == *" << endl;
-	cout << "배열은 포인터다" << endl;
-	cout << endl;
-	const char* str[3] = {"df", "ee", "gwegwe"};
-	const char **ptt = str;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < str[i][j]; j++) {
-			cout << str[i][j];
-		}
-		cout << endl;
+	while (c <= size) {
+		if (c < size && arr[c] > arr[c + 1]) c++;
+		if (temp <= arr[c]) break;
+		arr[p] = arr[c];
+		p = c;
+		c *= 2;
 	}
+	arr[p] = temp;
+}
 
-	cout << endl;
-	cout << "이중 포인터" << endl;
-	printf("typeid(str) = %s\n", typeid(str).name());
-	printf("typeid(str[0]) = %s\n", typeid(str[0]).name());
-	printf("typeid(&str[0]) = %s\n", typeid(&str[0]).name());
-	printf("typeid(ptt) = %s\n", typeid(ptt).name());
-	
-	printf("ptt[0] = %s\n", ptt[0]);
 
+int main() {
+	Heap<int> iheap;
+	cout << iheap.getSize() << endl;
+	iheap.push(3);
+	cout << iheap.top() << endl;
+	iheap.push(10);
+	cout << iheap.top() << endl;
+	iheap.push(2);
+	cout << iheap.top() << endl;
+	iheap.push(11);
+	cout << iheap.top() << endl;
+	iheap.pop();
+	cout << iheap.top() << endl;
 	return 0;
 }
