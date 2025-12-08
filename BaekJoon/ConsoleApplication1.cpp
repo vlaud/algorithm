4b31f6edcadea7636c3d22921916babb280f6a7e
vlaud <whehdgus1207@naver.com>
2025-12-08 13:19:52 +0900

    백준 34817 쉬운 정렬
    왼쪽부터 최대값 갱신
    현재 누적 최대값 > 현재 원소 + k면 정렬 불가능

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,80 +1,30 @@
 ﻿#include <iostream>
+#include <algorithm>
 using namespace std;
 
 #define endl '\n'
-#define mx 1000005
-
-template<typename T>
-class Heap {
-private:
-	T* arr;
-	int size = 0;
-public:
-	Heap();
-	~Heap();
-	void push(T val);
-	T top();
-	int getSize();
-	void pop();
-};
-template<typename T>
-Heap<T>::Heap() {
-	arr = new T[mx];
-}
-template<typename T>
-Heap<T>::~Heap() {
-	delete[] arr;
-	arr = nullptr;
-}
-template<typename T>
-void Heap<T>::push(T val) {
-	int i = ++size;
-
-	// 최소 힙
-	while (i != 1 && arr[i / 2] > val) {
-		arr[i] = arr[i / 2];
-		i /= 2;
-	}
-	arr[i] = val;
-}
-template<typename T>
-T Heap<T>::top() {
-	return arr[1];
-}
-template<typename T>
-int Heap<T>::getSize() {
-	return size;
-}
-template<typename T>
-void Heap<T>::pop() {
-	if (!size) return;
-
-	T temp = arr[size--];
-	int p = 1, c = 2;
-
-	while (c <= size) {
-		if (c < size && arr[c] > arr[c + 1]) c++;
-		if (temp <= arr[c]) break;
-		arr[p] = arr[c];
-		p = c;
-		c *= 2;
-	}
-	arr[p] = temp;
-}
-
 
 int main() {
-	Heap<int> iheap;
-	cout << iheap.getSize() << endl;
-	iheap.push(3);
-	cout << iheap.top() << endl;
-	iheap.push(10);
-	cout << iheap.top() << endl;
-	iheap.push(2);
-	cout << iheap.top() << endl;
-	iheap.push(11);
-	cout << iheap.top() << endl;
-	iheap.pop();
-	cout << iheap.top() << endl;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, k;
+
+	cin >> n >> k;
+
+
+	int mx = 0;
+
+	while (--n) {
+		int x; cin >> x;
+		if (mx > x + k) {
+			cout << "NO" << endl;
+			return 0;
+		}
+		mx = max(x, mx);
+	}
+
+	cout << "YES" << endl;
 	return 0;
 }
