6a03c249e59b5f84ad472197f92b96e6f316e506
vlaud <whehdgus1207@naver.com>
2025-12-15 16:35:27 +0900

    백준 4949: 균형잡힌 세상

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,67 +1,48 @@
 ﻿#include <iostream>
-#include <vector>
 #include <string>
-#include <queue>
-#include <sstream>
+#include <stack>
 
 using namespace std;
 
 #define endl '\n'
 
-vector<int> grp[26];
-vector<int> idg(26);
-vector<int> days(26);
-vector<long long> cache(26);
-
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
-    string j;
-    
-    while (true) {
-        getline(cin, j);
+	string j;
+	while (true) {
+		getline(cin, j);
+		if (j == ".") break;
+		stack<char> st;
 
-        if (j.empty()) break;
+		bool v = true;
+		for (char c : j) {
+			if (c == '[' || c == '(') {
+				st.emplace(c);
+				continue;
+			}
 
-        stringstream ss(j);
-        string tmp;
-        vector<string> strs;
-
-        while (getline(ss, tmp, ' ')) {
-            strs.emplace_back(tmp);
-        }
-        int num = strs[0][0] - 'A';
-        days[num] = stoi(strs[1]);
-
-        if (strs.size() == 2) continue;
-
-        for (char& c : strs[2]) {
-            c -= 'A';
-            grp[c].emplace_back(num);
-            idg[num]++;
-        }
-    }
-    queue<int> q;
-    long long res = 0;
-
-    for (int i = 0; i < 26; i++) {
-        if (!idg[i] && days[i]) {
-            cache[i] = static_cast<long long>(days[i]);
-            res = max(res, cache[i]);
-            q.emplace(i);
-        }
-    }
-
-    while (!q.empty()) {
-        int x = q.front(); q.pop();
-
-        for (int& i : grp[x]) {
-            cache[i] = max(cache[i], cache[x] + static_cast<long long>(days[i]));
-            res = max(res, cache[i]);
-            if (--idg[i] == 0) q.emplace(i);
-        }
-    }
-    cout << res << endl;
-    return 0;
+			if (c == ']' || c == ')') {
+				if (st.empty()) {
+					v = false;
+					break;
+				}
+				else if (c == ']' && st.top() != '[') {
+					v = false;
+					break;
+				}
+				else if (c == ')' && st.top() != '(') {
+					v = false;
+					break;
+				}
+				st.pop();
+			}
+			
+		}
+		if (!st.empty()) v = false;
+		string res = v ? "yes" : "no";
+		cout << res << endl;
+	}
+	return 0;
 }
