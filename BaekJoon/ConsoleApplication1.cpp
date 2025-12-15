c4bc153dfa4413c0476785ea1e559c6efac2271a
vlaud <whehdgus1207@naver.com>
2025-12-15 17:55:21 +0900

    백준 1926: 그림
    bfs

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,48 +1,64 @@
 ﻿#include <iostream>
-#include <string>
-#include <stack>
+#include <queue>
 
 using namespace std;
-
+using iPair = pair<int, int>;
 #define endl '\n'
 
+int board[500][500];
+
+int bfs(int row, int col, int n, int m) {
+	queue<iPair> q;
+
+	q.emplace(row, col);
+	board[row][col] = 0;
+	int size = 0;
+
+	while (!q.empty()) {
+		auto [r, c] = q.front(); q.pop();
+		
+		size++;
+		
+		for (int i = -1; i < 2; i += 2) {
+			int nr = r + i;
+			int nc = c + i;
+			if (0 <= nr && nr < n && board[nr][c]) {
+				q.emplace(nr, c);
+				board[nr][c] = 0;
+			}
+			if (0 <= nc && nc < m && board[r][nc]) {
+				q.emplace(r, nc);
+				board[r][nc] = 0;
+			}
+		}
+	}
+	return size;
+}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	string j;
-	while (true) {
-		getline(cin, j);
-		if (j == ".") break;
-		stack<char> st;
+	int n, m;
 
-		bool v = true;
-		for (char c : j) {
-			if (c == '[' || c == '(') {
-				st.emplace(c);
-				continue;
-			}
+	cin >> n >> m;
 
-			if (c == ']' || c == ')') {
-				if (st.empty()) {
-					v = false;
-					break;
-				}
-				else if (c == ']' && st.top() != '[') {
-					v = false;
-					break;
-				}
-				else if (c == ')' && st.top() != '(') {
-					v = false;
-					break;
-				}
-				st.pop();
-			}
-			
+	int count = 0, res = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
 		}
-		if (!st.empty()) v = false;
-		string res = v ? "yes" : "no";
-		cout << res << endl;
 	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!board[i][j]) continue;
+			count++;
+			int a = bfs(i, j, n, m);
+			res = max(res, a);
+		}
+	}
+
+	cout << count << endl;
+	cout << res << endl;
 	return 0;
 }
