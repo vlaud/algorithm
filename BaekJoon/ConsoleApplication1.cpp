702a0e61ec05e8562dc352b5488c5fd48d2f1356
vlaud <whehdgus1207@naver.com>
2025-12-15 15:17:13 +0900

    백준 14907: 프로젝트 스케줄링
    위상정렬, 문자열 받기, 캐시
    
    문자열을 통째로 입력받아 stringstream으로 분할
    문자열 벡터의 크기가 2면 진입차수는 0이다
    
    진입차수가 0이고, 걸리는 시간이 0보다 큰 것들만
    힙에 삽입하고, 누적 시간 초기화, 최소 시간을 갱신
    
    힙이 빌 때까지, 현재 노드에 연결된 노드들의 누적 시간을 갱신하고, 최소 시간도 갱신한다.
    진입 차수가 0이 되면 힙에 넣는다

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,35 +1,67 @@
 ﻿#include <iostream>
-#include <stack>
+#include <vector>
+#include <string>
+#include <queue>
+#include <sstream>
+
 using namespace std;
 
 #define endl '\n'
-#define ll long long
+
+vector<int> grp[26];
+vector<int> idg(26);
+vector<int> days(26);
+vector<long long> cache(26);
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    int n;
-
+    string j;
+    
     while (true) {
-        cin >> n;
-        if (!n) break;
+        getline(cin, j);
 
-        stack<pair<int,int>> st;
-        ll size = 0;
-        for (int i = 0; i <= n; i++) {
-            int num;
-            if (i != n) cin >> num;
-            while (!st.empty() && (i == n || st.top().first > num)) {
-                ll h = st.top().first; st.pop();
-                int w = st.empty() ? i : i - st.top().second - 1;
-                size = max(size, h * w);
-            }
-            
-            st.emplace(pair<int,int>({ num, i }));
+        if (j.empty()) break;
+
+        stringstream ss(j);
+        string tmp;
+        vector<string> strs;
+
+        while (getline(ss, tmp, ' ')) {
+            strs.emplace_back(tmp);
         }
-        cout << size << endl;
+        int num = strs[0][0] - 'A';
+        days[num] = stoi(strs[1]);
+
+        if (strs.size() == 2) continue;
+
+        for (char& c : strs[2]) {
+            c -= 'A';
+            grp[c].emplace_back(num);
+            idg[num]++;
+        }
+    }
+    queue<int> q;
+    long long res = 0;
+
+    for (int i = 0; i < 26; i++) {
+        if (!idg[i] && days[i]) {
+            cache[i] = static_cast<long long>(days[i]);
+            res = max(res, cache[i]);
+            q.emplace(i);
+        }
     }
 
+    while (!q.empty()) {
+        int x = q.front(); q.pop();
+
+        for (int& i : grp[x]) {
+            cache[i] = max(cache[i], cache[x] + static_cast<long long>(days[i]));
+            res = max(res, cache[i]);
+            if (--idg[i] == 0) q.emplace(i);
+        }
+    }
+    cout << res << endl;
     return 0;
 }
