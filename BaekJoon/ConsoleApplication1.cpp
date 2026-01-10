acf29e053d063a575743c915365ae8027f598c6b
vlaud <whehdgus1207@naver.com>
2026-01-10 11:48:00 +0900

    세그먼트 트리 구현

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,149 +1,56 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-#define mx 10000
-
-class str
+class SegTree
 {
 private:
-	int size, max_size;
-	char* words;
-	void updateSize();
-	int getsize(const char* string);
-
+	int* tree;
 public:
-	str();
-	~str();
-
-	void operator=(const char* string);
-	void operator+=(const char* string);
-	bool operator==(const char* string);
-	char* wPtr();
-	int getsize();
+	SegTree(int n);
+	int init(int vect[], int l, int r, int node);
+	int findSum(int vect[], int l, int r, int begin, int end, int node);
+	void change(int vect[], int l, int r, int idx, int offset, int node);
 };
 
-str::str()
+SegTree::SegTree(int n)
 {
-	size = 0;
-	max_size = mx;
-	words = new char[max_size + 1];
+	tree = new int[n * 4];
 }
 
-str::~str()
+int SegTree::init(int vect[], int l, int r, int node)
 {
-	delete(words);
-	words = nullptr;
+	if (l == r) return tree[node] = vect[l];
+
+	int mid = l + (r - l) / 2;
+	return tree[node] = init(vect, l, mid, node * 2) + init(vect, mid + 1, r, node * 2 + 1);
 }
 
-void str::operator=(const char* string)
+int SegTree::findSum(int vect[], int l, int r, int begin, int end, int node)
 {
-	size = getsize(string);
-	updateSize();
+	if (end < l || r < begin) return 0;
+	if (begin <= l && r <= end) return tree[node];
 
-	for (int i = 0; i < size; i++)
-	{
-		words[i] = string[i];
-	}
+	int mid = l + (r - l) / 2;
+	return findSum(vect, l, mid, begin, end, node * 2) + findSum(vect, mid + 1, r, begin, end, node * 2 + 1);
 }
 
-void str::operator+=(const char* string)
+void SegTree::change(int vect[], int l, int r, int idx, int offset, int node)
 {
-	char* old = words;
-	int oldsize = size;
-	size += getsize(string);
-
-	updateSize();
-
-	for (int i = 0; i < oldsize; i++)
-	{
-		words[i] = old[i];
-	}
-	for (int i = oldsize; i < size; i++)
-	{
-		words[i] = string[i - oldsize];
-	}
-}
-
-bool str::operator==(const char* string)
-{
-	if (size != getsize(string)) return false;
-
-	for (int i = 0; i < size; i++)
-	{
-		if (words[i] != string[i]) return false;
-	}
-
-	return true;
-}
-
-void str::updateSize()
-{
-	if (size <= max_size) return;
-	max_size = size;
-	words = new char[max_size + 1];
-}
-
-char* str::wPtr()
-{
-	return words;
-}
-
-int str::getsize(const char* string)
-{
-	int tsize = 0;
-	while (string[tsize] != '\0')
-	{
-		tsize++;
-	}
-	return tsize;
-}
-
-int str::getsize()
-{
-	return size;
-}
-
-ostream& operator<<(ostream& os, str& string)
-{
-	for (int i = 0; i < string.getsize(); i++)
-	{
-		os << string.wPtr()[i];
-	}
-	return os;
-}
-
-istream& operator>>(istream& is, str& string)
-{
-	char words[256] = {};
-	is >> words;
-	string = words;
-	return is;
+	if (idx < l || r < idx) return;
+	tree[node] += offset;
+	if (l == r) return;
+	int mid = l + (r - l) / 2;
+	change(vect, l, mid, idx, offset, node * 2);
+	change(vect, mid + 1, r, idx, offset, node * 2 + 1);
 }
 
 int main()
 {
-	str s;
-	s = "amorcito";
-	cout << s << "\n";
-
-	s += "love";
-	cout << s << "\n";
-
-	s = "love";
-	cout << s << "\n";
-
-	s += "you";
-	cout << s << "\n";
-
-	cout << (s == "loveyou") << endl;
-	cout << (s == "loveyo") << endl;
-	cout << (s == "loveytt") << endl;
-
-	cin >> s;
-	cout << s << "\n";
-
-	cin >> s;
-	cout << s << "\n";
+	int vect[8] = { 1,2,3,4,5,6,7,8 };
+	SegTree seg(8);
+	seg.init(vect, 0, 7, 1);
+	cout << seg.findSum(vect, 0, 7, 3, 6, 1) << endl;
+	seg.change(vect, 0, 7, 4, 100, 1);
+	cout << seg.findSum(vect, 0, 7, 3, 6, 1);
 	return 0;
 }
