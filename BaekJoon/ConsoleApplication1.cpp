63b6a615adf0e253ad3fe526fd1b3f36c3f2d436
vlaud <whehdgus1207@naver.com>
2026-01-12 18:38:07 +0900

    KMP 알고리즘

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,56 +1,57 @@
 ﻿#include <iostream>
-
+#include <string>
+#include <vector>
 using namespace std;
-class SegTree
-{
-private:
-	int* tree;
-public:
-	SegTree(int n);
-	int init(int vect[], int l, int r, int node);
-	int findSum(int vect[], int l, int r, int begin, int end, int node);
-	void change(int vect[], int l, int r, int idx, int offset, int node);
-};
 
-SegTree::SegTree(int n)
+vector<int> makeTable(string pattern)
 {
-	tree = new int[n * 4];
+	const int n = pattern.size();
+	vector<int> table(n);
+
+	int len = 0;
+
+	for (int i = 1; i < n; i++)
+	{
+		while (0 < len && pattern[i] != pattern[len])
+		{
+			len = table[len - 1];
+		}
+		if (pattern[i] == pattern[len])
+		{
+			table[i] = ++len;
+		}
+	}
+	return table;
 }
 
-int SegTree::init(int vect[], int l, int r, int node)
+void KMP(string parent, string pattern)
 {
-	if (l == r) return tree[node] = vect[l];
+	vector<int> table = makeTable(pattern);
+	const int parentSize = parent.size(), patternSize = pattern.size();
 
-	int mid = l + (r - l) / 2;
-	return tree[node] = init(vect, l, mid, node * 2) + init(vect, mid + 1, r, node * 2 + 1);
+	int len = 0;
+
+	for (int i = 0; i < parentSize; i++)
+	{
+		while (0 < len && parent[i] != pattern[len])
+		{
+			len = table[len - 1];
+		}
+		if (parent[i] == pattern[len])
+		{
+			if (len == patternSize-1)
+			{
+				printf("%d에서 찾음\n", i - patternSize + 2);
+				len = table[len];
+			}
+			else len++;
+		}
+	}
 }
-
-int SegTree::findSum(int vect[], int l, int r, int begin, int end, int node)
-{
-	if (end < l || r < begin) return 0;
-	if (begin <= l && r <= end) return tree[node];
-
-	int mid = l + (r - l) / 2;
-	return findSum(vect, l, mid, begin, end, node * 2) + findSum(vect, mid + 1, r, begin, end, node * 2 + 1);
-}
-
-void SegTree::change(int vect[], int l, int r, int idx, int offset, int node)
-{
-	if (idx < l || r < idx) return;
-	tree[node] += offset;
-	if (l == r) return;
-	int mid = l + (r - l) / 2;
-	change(vect, l, mid, idx, offset, node * 2);
-	change(vect, mid + 1, r, idx, offset, node * 2 + 1);
-}
-
 int main()
 {
-	int vect[8] = { 1,2,3,4,5,6,7,8 };
-	SegTree seg(8);
-	seg.init(vect, 0, 7, 1);
-	cout << seg.findSum(vect, 0, 7, 3, 6, 1) << endl;
-	seg.change(vect, 0, 7, 4, 100, 1);
-	cout << seg.findSum(vect, 0, 7, 3, 6, 1);
+	string parent = "ababacabacaabacaaba";
+	string pattern = "abacaaba";
+	KMP(parent, pattern);
 	return 0;
 }
