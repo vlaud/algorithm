64e5c6657b4890300477d364fc9a9d4709cefde6
vlaud <whehdgus1207@naver.com>
2026-03-03 12:40:42 +0900

    ksum
    2sum:
    l과 r의 합계
    합계가 타깃과 같으면 [l],[r] 배열 생성, 삽입 후 l++, r--
    l,r의 이전 원소와 중복이면 l++, r--
    
    합 < 타깃 => l++
    합 > 타깃 => r--
    
    ksum:
    검사할 원소가 더 이상 없음(s==n) => 빈 배열 리턴
    평균이 가장 작은 원소보다 작거나, 가장 큰 원소보다 큼 => 빈 배열 리턴
    
    k가 2면 2sum 리턴
    
    i = (s~n-1)
    [i] 이전 원소와 중복이면 스킵
    
    for (서브배열 : ksum(i+1, k-1, 타깃-[i]))
    새 배열에 [i]삽입 후, [i] 뒤에 서브배열 붙이기

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,132 +1,72 @@
 ﻿#include <iostream>
 #include <vector>
-#include <numeric>
-#include <unordered_map>
 #include <algorithm>
-
 using namespace std;
-using str2d = vector<vector<string>>;
 
-/// <summary>
-/// 서로소 집합
-/// </summary>
-class dsu
+using int2d = vector<vector<int>>;
+using ll = long long;
+
+int2d twoSum(vector<int> arr, int s, int n, ll target)
 {
-private:
-	vector<int> par, size;
-public:
-	/// <summary>
-	/// n의 크기만큼 각 원소들의 집합 생성
-	/// 각 부모의 원소는 자기 자신, 크기는 1로 초기화
-	/// </summary>
-	/// <param name="n"></param>
-	dsu(int n)
-	{
-		par.resize(n);
-		iota(begin(par), end(par), 0);
-		size.resize(n, 1);
-	}
+	int2d res;
+	int l = s, r = n - 1;
 
-	/// <summary>
-	/// 원소의 부모 찾기
-	/// </summary>
-	/// <param name="x"></param>
-	/// <returns></returns>
-	int find(int x)
+	while (l < r)
 	{
-		while (x != par[x])
+		ll sum = arr[l] + arr[r];
+		if (sum == target)
 		{
-			par[x] = par[par[x]];
-			x = par[x];
+			res.emplace_back(vector<int>({ arr[l++], arr[r--] }));
+			while (l < r && arr[l] == arr[l - 1]) l++;
+			while (l < r && arr[r] == arr[r + 1]) r--;
 		}
-		return x;
+		else if (sum < target) l++;
+		else r--;
 	}
+	return res;
+}
+int2d kSum(vector<int> arr, int s, int n, int k, ll target)
+{
+	int2d res;
 
-	/// <summary>
-	/// a와 b를 합친 집합
-	/// </summary>
-	/// <param name="a"></param>
-	/// <param name="b"></param>
-	void merge(int a, int b)
+	if (s == n) return res;
+
+	ll average = target / k;
+
+	if (average < arr[s] || arr.back() < average) return res;
+
+	if (k == 2) return twoSum(arr, s, n, target);
+
+	for (int i = s; i < n; i++)
 	{
-		a = find(a);
-		b = find(b);
+		if (s != i && arr[i] == arr[i - 1]) continue;
 
-		if (a == b) return;
-
-		if (size[a] < size[b])
+		for (auto& sub : kSum(arr, i + 1, n, k - 1, static_cast<ll>(target - arr[i])))
 		{
-			swap(a, b);
+			res.emplace_back(vector<int>({ arr[i] }));
+			res.back().insert(res.back().end(), sub.begin(), sub.end());
 		}
-
-		par[b] = a;
-		size[a] += size[b];
 	}
-};
 
+	return res;
+}
 
+int2d GetKSum(vector<int> arr, int k, ll target)
+{
+	sort(arr.begin(), arr.end());
+	return kSum(arr, 0, arr.size(), k, target);
+}
 int main()
 {
-	str2d emails =
+	vector<int> arr = { 1,0,-1,0,-2,2 };
+
+	for (auto& sub : GetKSum(arr, 4, 0))
 	{
-		{"tom", "tom@aa", "tom@bb"},
-		{"jin", "jin@cc", "jin@dd"},
-		{"tom", "tom@cc", "tom@dd"},
-		{"jin", "jin@ss", "jin@tt"},
-		{"tom", "tom@bb", "tom@cc"},
-	};
-
-	int n = emails.size();
-
-	unordered_map<string, int> mp;
-	dsu ds(n);
-
-	
-	for (int i = 0; i < n; i++)
-	{
-		// 이름 (0번 원소는) 스킵
-		for (int j = 1; j < emails[i].size(); j++)
+		for (auto& i : sub)
 		{
-			// 해시맵에 이메일이 있으면 병합
-			// 없으면 i 인덱스 (이메일 주인 인덱스)를 삽입
-			auto& e = emails[i][j];
-			if (mp.find(e) != mp.end()) ds.merge(mp[e], i);
-			else mp[e] = i;
-		}
-	}
-
-	// 임시 2차원 벡터 생성
-	str2d temp(n);
-	for (auto [e, i] : mp)
-	{
-		// 해시맵에 저장된 인덱스의 부모 찾기
-		int par = ds.find(i);
-
-		// 부모 인덱스 위치에 이메일 삽입
-		temp[par].emplace_back(e);
-	}
-
-	str2d res;
-	for (int i = 0; i < n; i++)
-	{
-		if (temp[i].empty()) continue;
-
-		// 이메일을 정렬
-		sort(begin(temp[i]), end(temp[i]));
-
-		// 이메일 주인의 이름을 먼저 삽입 후 소유한 이메일들을 뒤에 붙인다
-		res.emplace_back(vector<string>({ emails[i][0] }));
-		res.back().insert(res.back().end(), temp[i].begin(), temp[i].end());
-	}
-
-	for (auto& e : res)
-	{
-		for (string& s : e)
-		{
-			printf("%s ", s.c_str());
+			printf("%d ", i);
 		}
 		cout << endl;
 	}
-
 	return 0;
 }
