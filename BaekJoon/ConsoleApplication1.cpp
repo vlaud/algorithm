3635c8364dca7800d3568ecc17fbb0388f9d3d9c
vlaud <whehdgus1207@naver.com>
2026-03-09 18:28:26 +0900

    A* 알고리즘 구현
    A* = 다익스트라 + 휴리스틱(큐에 삽입)

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -12,7 +12,7 @@ using distAndCoord = tuple<double, int, int>;
 
 enum NODE
 {
-	A,B,C,D,E,F,G
+	A, B, C, D, E, F, G
 };
 void ConnectNode(vector<distAndCoord>& roads, diPair2D& graph)
 {
@@ -23,7 +23,7 @@ void ConnectNode(vector<distAndCoord>& roads, diPair2D& graph)
 	}
 }
 
-double FindShortest(diPair2D& graph, int start, int end)
+double FindShortest(diPair2D& graph, int start, int end, vector<double>& h)
 {
 	priority_queue<diPair, vector<diPair>, greater<diPair>> q;
 	vector<double> dists(SIZE, DBL_MAX); // 누적거리 배열, 시작 지점 제외 전부 INF로 고정
@@ -31,35 +31,48 @@ double FindShortest(diPair2D& graph, int start, int end)
 
 	dists[start] = 0.0; // 시작 지점 거리 = 0
 
-	q.emplace(0, start);
+	q.emplace(h[start], start);
 
 	while (!q.empty())
 	{
-		auto [dist, u] = q.top();
+		auto [f, u] = q.top();
 		q.pop();
 
-		if (u == end) break; // 다익스트라 알고리즘과의 유일한 차이 => 목적지에 도달하면 break
-		if (dist > dists[u]) continue;
+		printf("현재 정점: %c\n", u + 'A');
+		if (u == end) // 다익스트라 알고리즘과의 유일한 차이 => 목적지에 도달하면 break
+		{
+			printf("도착 완료\n");
+			break;
+		}
+		printf("누적 거리: %lf\n", dists[u]);
+		printf("도착까지 추정 거리: %lf\n", h[u]);
 
 		for (auto [d, node] : graph[u])
 		{
-			if (dists[node] > d + dists[u]) // 기존 누적 거리 > 현재 노드 누적 거리 + 다음 노드 거리면 갱신
+			double newG = d + dists[u];
+			if (dists[node] > newG) // 기존 누적 거리 > 현재 노드 누적 거리 + 다음 노드 거리면 갱신
 			{
-				dists[node] = d + dists[u];
+				
+				printf("%c까지 거리: %lf\n", node + 'A', d);
+				printf("%c에서 도착까지 추정 거리: %lf\n", node + 'A', h[node]);
+				dists[node] = newG;
 				prevs[node] = u;
-				q.emplace(dists[node], node);
+
+				double fScore = newG + h[node];
+				q.emplace(fScore, node);
 			}
 		}
+		cout << endl;
 	}
 
 	int arrive = end;
 
-	printf("역추적: %c ", arrive+'A');
+	printf("역추적: %c ", arrive + 'A');
 	arrive = prevs[arrive];
 
 	while (arrive != -1)
 	{
-		printf("- %c ", arrive+'A');
+		printf("- %c ", arrive + 'A');
 		arrive = prevs[arrive];
 	}
 	cout << endl;
@@ -83,9 +96,19 @@ int main()
 	};
 
 	diPair2D graph(SIZE);
+	vector<double> h // G까지의 휴리스틱 경로
+	{
+		9.0, // A
+		8.5, // B
+		5.7,
+		4.0,
+		2.5,
+		1.6,
+		0.0
+	};
 
 	ConnectNode(roads, graph);
 
-	cout << FindShortest(graph, NODE::E, NODE::A);
+	cout << FindShortest(graph, NODE::A, NODE::G, h);
 	return 0;
 }
