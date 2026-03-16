8cb2386523af82eca8d89e56e425f4284a83b1f3
vlaud <whehdgus1207@naver.com>
2026-03-16 19:43:27 +0900

    2차원 배열 동적 할당과 메모리 해제
    동적할당:
    int** pptr = new int*[3] // 이중 포인터에 포인터 배열 동적 할당
    
    for (0~n-1) pptr[i] = new int[m] 각 포인터 배열 원소에 배열 동적 할당
    
    메모리 해제
    
    순서 거꾸로
    
    for(0~n-1) delete[] pptr[i]
    delete[] pptr

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,145 +1,26 @@
 ﻿#include <iostream>
-#include <unordered_set>
 
 using namespace std;
 
-class ITest
+void tt()
 {
-public:
-	virtual void test() = 0;
-};
+	int** test = new int* [3];
 
-class IComponent
-{
-public:
-	virtual void Execute() = 0;
-};
-
-class Test : public IComponent, public ITest
-{
-public:
-	void Execute() override
+	for (int i = 0; i < 3; i++)
 	{
-		test();
+		test[i] = new int[3];
 	}
 
-	void test() override
+
+	for (int i = 0; i < 3; i++)
 	{
-		cout << "테스트" << endl;
+		delete[] test[i];
 	}
-
-};
-class ICamera
-{
-public:
-	virtual void Rotate() = 0;
-};
-
-class Camera : public IComponent, public ICamera
-{
-public:
-	Camera() {}
-	void Rotate() override
-	{
-		cout << "돌기" << endl;
-	}
-	void Execute() override
-	{
-		Rotate();
-	}
-};
-
-class GameObeject : public IComponent
-{
-public:
-	GameObeject() : cam(nullptr), _components(), camObject(nullptr)
-	{
-		
-	}
-
-	void Execute() override
-	{
-		for (auto& comp : _components)
-		{
-			comp->Execute();
-		}
-	}
-
-	void AddCamera(IComponent* component)
-	{
-		if (ICamera* cm = dynamic_cast<ICamera*>(component))
-		{
-			camObject = component;
-			cam = cm;
-		}
-	}
-
-	void AddComponent(IComponent* component)
-	{
-		if (_components.find(component) != _components.end())
-		{
-			cout << "이미 있음" << endl;
-			return;
-		}
-
-		_components.insert(component);
-	}
-
-	template<class T>
-	T* GetComponent()
-	{
-		for (auto& comp : _components)
-		{
-			if (T* type = dynamic_cast<T*>(comp))
-			{
-				return type;
-			}
-		}
-
-		cout << "해당 컴포넌트 없음" << endl;
-		return nullptr;
-	}
-
-	void Rotate()
-	{
-		if (!cam)
-		{
-			cout << "카메라 없음" << endl;
-			return;
-		}
-		cam->Rotate();
-	}
-
-private:
-	unordered_set<IComponent*> _components;
-	ICamera* cam;
-	IComponent* camObject;
-};
+	delete[] test;
+}
 
 int main()
 {
-	GameObeject* player = new GameObeject();
-	player->Rotate();
-	player->Execute();
-
-	Test* test = new Test();
-	player->AddComponent(test);
-	player->Execute();
-
-	Test* test1 = new Test();
-	player->AddComponent(test);
-	player->Execute();
-
-	Camera* cam = new Camera();
-	player->AddComponent(cam);
-	player->Execute();
-	player->AddCamera(cam);
-
-	player->Rotate();
-	player->GetComponent<ITest>()->test();
-	player->GetComponent<ICamera>()->Rotate();
-	player->GetComponent<Camera>()->Rotate();
-
-	// player->GetComponent<T>가 nullptr이면 에러
+	tt();
 	return 0;
 }
