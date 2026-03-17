78e7a11a480e01b94305e7598b2f1238be5d6e4d
vlaud <whehdgus1207@naver.com>
2026-03-17 13:25:49 +0900

    템플릿 설명
    변수 템플릿, 명시적 특수화, 가변인자 템플릿 폴드 방식

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,26 +1,55 @@
 ﻿#include <iostream>
+#include <print>
 
-using namespace std;
 
-void tt()
+/* 변수 템플릿 */
+template<typename T>
+constexpr T pi = T(3.1415926535897932385L);
+
+/* 명시적 특수화 */
+template<typename T>
+void Swap(T& a, T& b)
 {
-	int** test = new int* [3];
+	T temp = a;
+	a = b;
+	b = temp;
+}
 
-	for (int i = 0; i < 3; i++)
-	{
-		test[i] = new int[3];
-	}
+template<>
+void Swap<double>(double& a, double& b)
+{
+	std::cout << "double은 스왑 x" << std::endl;
+}
 
 
-	for (int i = 0; i < 3; i++)
-	{
-		delete[] test[i];
-	}
-	delete[] test;
+/* 가변 인자 템플릿 - 폴드 방식 C++17 */
+
+template<typename... Args>
+void print(Args... args)
+{
+	//(std::print("{} ", args), ...);
+	(std::cout << ... << args);
+	std::print("\n");
+}
+
+template<typename... Args>
+auto sum(Args... args)
+{
+	return (... + args);
 }
 
 int main()
 {
-	tt();
+	int x = 10, y = 20;
+	Swap(x, y);
+
+	double a = 1.5, b = 2.5;
+
+	Swap(a, b);
+
+	print(1, 2, 'A', "dfewge");
+
+	std::cout << sum(1, 2, 3, 4, 5);
+
 	return 0;
 }
