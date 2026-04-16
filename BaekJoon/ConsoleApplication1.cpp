a9756583adb3d5bb8ea46fb1681f257c6bc64bc6
vlaud <whehdgus1207@naver.com>
2026-04-16 12:17:23 +0900

    0/1 배낭 문제
    dp[물건 가지 수][제한 무게] = 제한 무게 내에서 물건 가지 수만큼 물건을 선택할 시 최대 가치

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,42 +1,36 @@
 ﻿#include <iostream>
-
+#include <vector>
 using namespace std;
 
-int arr[6] = { 1,5,4,2,-5,-7 };
-
-int partition(int l, int r)
-{
-	int p = arr[r];
-
-	for (int i = l; i < r; i++)
-	{
-		if (p >= arr[i])
-		{
-			swap(arr[l++], arr[i]);
-		}
-	}
-	swap(arr[l], arr[r]);
-
-	return l;
-}
-
-int kSelect(int l, int r, int k)
-{
-	while (l <= r)
-	{
-		int m = partition(l, r);
-		if (k == m) return m;
-		else if (m < k) l = m + 1;
-		else r = m - 1;
-	}
-	return -1;
-}
 int main()
 {
-	int n;
-	cin >> n;
-	n = 6 - n;
-	int idx = kSelect(0, 5, n);
-	cout << arr[idx];
+	int p[4] = { 15,20,9,14 };
+	int w[4] = { 3,5,3,4 };
+	int m = 10;
+	vector<vector<double>> dp(5, vector<double>(m + 1)); // 물품 수, 무게
+
+	//dp[0][0~m] = 물건을 하나도 선택 안함 -> 전부 0
+	//dp[i] i번째 물건을 고려함
+	for (int i = 1; i <= 4; i++)
+	{
+		int curw = w[i - 1];
+		int curval = p[i - 1];
+
+		for (int j = 1; j <= m; j++)
+		{
+			// 현재 짐을 넣거나 dp[i-1][j-curw] + curval
+			// 넣치 않거나 dp[i-1][j]
+
+			// 현재 무게 j가 물체의 무게 curw 이상일 때만 계산
+			if (curw <= j)
+			{
+				// 이전 물체의 최대 무게 계산 vs 
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - curw] + curval);
+			}
+			else dp[i][j] = dp[i - 1][j];
+		}
+	}
+
+	cout << dp[4][10];
 	return 0;
 }
