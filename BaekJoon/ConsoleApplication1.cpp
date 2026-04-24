c15fa335c0780835be4a307c1fe862a959cac9dc
vlaud <whehdgus1207@naver.com>
2026-04-24 18:12:03 +0900

    벨만 포드 알고리즘
    1~n-1 간선 개수 확인
    이전에 거리 갱신한 from 노드 소속 간선 중 to를 향한 거리가 [from]+weight보다 크면 갱신
    
    다 끝내고 모든 간선에서 [to] > [from]+weight가 존재하면 음수 사이클 발생

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -2,35 +2,55 @@
 #include <vector>
 using namespace std;
 
-int main()
+class edge
 {
-	int p[4] = { 15,20,9,14 };
-	int w[4] = { 3,5,3,4 };
-	int m = 10;
-	vector<vector<double>> dp(5, vector<double>(m + 1)); // 물품 수, 무게
+public:
+	int weight, from, to;
+	edge(int weight, int from, int to) : weight(weight), from(from), to(to) {}
 
-	//dp[0][0~m] = 물건을 하나도 선택 안함 -> 전부 0
-	//dp[i] i번째 물건을 고려함
-	for (int i = 1; i <= 4; i++)
+	bool operator< (const edge& other) const
 	{
-		int curw = w[i - 1];
-		int curval = p[i - 1];
+		weight < other.weight;
+	}
+};
 
-		for (int j = 1; j <= m; j++)
+void bellman(vector<edge>& edges,int s, int maxNode)
+{
+	int n = edges.size();
+	vector<int> dist(maxNode+1, INT_MAX);
+	dist[s] = 0;
+
+	for (int i = 1; i < n; i++)
+	{
+		for (auto& e : edges)
 		{
-			// 현재 짐을 넣거나 dp[i-1][j-curw] + curval
-			// 넣치 않거나 dp[i-1][j]
+			int from = e.from;
+			int weight = e.weight;
+			int to = e.to;
 
-			// 현재 무게 j가 물체의 무게 curw 이상일 때만 계산
-			if (curw <= j)
+			if (dist[from] != INT_MAX && dist[to] > dist[from] + weight)
 			{
-				// 이전 물체의 최대 무게 계산 vs 
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - curw] + curval);
+				dist[to] = dist[from] + weight;
 			}
-			else dp[i][j] = dp[i - 1][j];
 		}
 	}
 
-	cout << dp[4][10];
+	for (auto& e : edges)
+	{
+		if (dist[e.from] != INT_MAX && dist[e.to] > dist[e.from] + e.weight)
+		{
+			cout << "음수 사이클 존재" << endl;
+		}
+	}
+}
+int main()
+{
+	vector<edge> edges;
+
+	edges.emplace_back(edge(4, 1, 2));
+	edges.emplace_back(edge(2, 1, 3));
+	edges.emplace_back(edge(-3, 2, 3));
+	edges.emplace_back(edge(1, 3, 4));
+	bellman(edges, 1, 4);
 	return 0;
 }
