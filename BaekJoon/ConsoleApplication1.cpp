095993c4f6d409e9309bfd54f73997805d775b3e
vlaud <whehdgus1207@naver.com>
2026-05-27 14:39:34 +0900

    퍼즐 풀기 + 해시셋 구현
    퍼즐의 상태 -> string으로 변환 -> 해시셋에 저장
    체이닝 형태로 충돌 방지

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,96 +1,182 @@
 ﻿#include <iostream>
+#include <string>
 #include <vector>
+#include <list>
 
 using namespace std;
 
-vector<int> bad_char(string& pattern)
-{
-	int n = pattern.size();
+#define PRIME 7919
 
-	vector<int> bd(256, n);
+int res = 7;
+
+int ymin = 1, xmin = 1;
+int ymax = INT_MAX, xmax = INT_MAX;
+
+struct HASH
+{
+	string key;
+
+	HASH(string key) : key(key) {}
+};
+
+list<HASH*> hmap[PRIME];
+
+uint64_t gethash(string& s)
+{
+	uint64_t hash = 5381;
+
+	for (char c : s)
+	{
+		hash = (hash << 5) + hash + c;
+	}
+
+	return hash;
+}
+
+bool exist(string& s, int& h)
+{
+	if (hmap[h].empty()) return false;
+
+	for (auto it = hmap[h].begin(); it != hmap[h].end(); it++)
+	{
+		if ((*it)->key == s) return true;
+	}
+	return false;
+}
+
+void insert(string& s, int& h)
+{
+	hmap[h].emplace_back(new HASH(s));
+}
+
+string vecToString(vector<string>& mat)
+{
+	string s;
+	for (string c : mat)
+	{
+		s += c;
+	}
+	return s;
+}
+
+vector<int> maketable(string& pat)
+{
+	int n = pat.size();
+	vector<int> table(n, 0);
+
+	int len = 0;
+
+	for (int i = 1; i < n; i++)
+	{
+		while (len != 0 && pat[len] != pat[i]) len = table[len - 1];
+
+		if (pat[len] == pat[i])
+		{
+			table[i] = ++len;
+		}
+	}
+	return table;
+}
+
+bool search(string& s, string& pat, vector<int>& table)
+{
+	int n = s.size();
+	int m = pat.size();
+	int len = 0;
 
 	for (int i = 0; i < n; i++)
 	{
-		bd[pattern[i]] = i;
+		while (len != 0 && pat[len] != s[i]) len = table[len - 1];
+
+		if (pat[len] == s[i])
+		{
+			if (len == m - 1) return true;
+			else len++;
+		}
 	}
-	return bd;
+
+	return false;
 }
 
-vector<int> make_good(string& pattern)
+void spin(vector<string>& mat, int r, int c, bool right = true)
 {
-	int n = pattern.size();
+	int n = mat.size(), m = mat[0].size();
 
-	int p = n;
-	int sp = p + 1;
-
-	// 접미사 동일 문자열 체크 테이블
-	vector<int> suffix(n + 1, 0);
-	suffix[n] = sp;
-	// 스킵 테이블
-	vector<int> skip(n + 1, 0);
-
-	// 역 preKMP
-	while (p > 0)
+	if (right)
 	{
-		while (sp <= n && pattern[p - 1] != pattern[sp - 1])
+		for (int i = -1; i < 1; i++)
 		{
-			if (skip[sp] == 0) 
-				skip[sp] = sp - p;
-			sp = suffix[sp];
+			for (int j = i + 1; j < 2; j++)
+			{
+				swap(mat[r + i][c + j], mat[r + j][c + i]);
+			}
 		}
-		suffix[--p] = --sp;
 	}
-
-	sp = suffix[0];
-
-	while (p < n)
+	else
 	{
-		if (skip[p] == 0)
-			skip[p] = sp;
-		if (p++ == sp)
-			sp = suffix[sp];
+		for (int i = -1; i < 1; i++)
+		{
+			for (int j = -1; j < -i; j++)
+			{
+				swap(mat[r + i][c + j], mat[r - j][c - i]);
+			}
+		}
 	}
 
-	return skip;
+	for (int i = r - 1; i <= r + 1; i++)
+	{
+		swap(mat[i][c - 1], mat[i][c + 1]);
+	}
 }
 
-void search(vector<int>& bad, vector<int>& good, string& s, string& pattern)
+void findcomb(vector<string>& mat, string& pat, vector<int>& table, int level, int cnt)
 {
-	int n = s.size(), m = pattern.size();
-
-	int b = 0;
-
-	if (n < m) return;
-
-	while (b <= n - m)
+	for (int i = 0; i < mat.size(); i++)
 	{
-		int match = m;
-
-		while (match != 0 && pattern[match - 1] == s[b + match - 1]) --match;
-
-		if (!match)
+		if (search(mat[i], pat, table))
 		{
-			cout << b << " ";
+			res = min(cnt, res);
+			return;
 		}
+	}
 
-		char c = s[b + match];
-		int shift = bad[c];
-		int goodshift = good[match];
-		if (shift != m)
+	string s = vecToString(mat);
+
+	int h = gethash(s) % PRIME;
+	if (level == 6 || exist(s, h)) return;
+
+	insert(s,h);
+	
+	for (int i = ymin; i <= ymax; i++)
+	{
+		for (int j = xmin; j <= xmax; j++)
 		{
-			b += max(match - shift, good[match]);
+			spin(mat, i, j);
+			findcomb(mat, pat, table, level + 1, cnt + 1);
+			spin(mat, i, j, false);
 		}
-		else b += max(match, good[match]);
 	}
 }
+
 int main()
 {
-	string h = "abztyaababbababttbcabbabatdwcaabbabczzba";
-	string pattern = "abbab";
-	const int p_size = pattern.size();
+	vector<string> mat;
 
-	vector<int> bad = bad_char(pattern);
-	vector<int> good = make_good(pattern);
-	search(bad, good, h, pattern);
+	string s;
+	while (true)
+	{
+		getline(cin, s);
+		if (s.empty()) break;
+		mat.emplace_back(s);
+	}
+	string pat = "AAA";
+	vector<int> table = maketable(pat);
+
+	ymax = mat.size() - 2;
+	xmax = mat[0].size() - 2;
+
+	findcomb(mat, pat, table, 0, 0);
+
+	cout << (res == 7 ? "불가능" : "가능") << endl;
 	return 0;
 }
