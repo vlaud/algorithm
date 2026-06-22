9b88a488b8deae09962a417fde8323acf3d66792
vlaud <whehdgus1207@naver.com>
2026-06-22 15:50:53 +0900

    다익스트라 1~4번 순서
    중복된 1~4 노드 차례대로 방문 -> 가장 최단거리

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,88 +1,90 @@
 ﻿#include <iostream>
+#include <vector>
 #include <string>
-#include <list>
+#include <queue>
+#include <functional>
 
 using namespace std;
 
-int res = 7;
+string mat[3];
 
-int ymin = 1, xmin = 1;
-int ymax = INT_MAX, xmax = INT_MAX;
-
-struct HASH
+struct Coord
 {
-	string key;
-	int depth;
-	HASH(string key, int level) : key(key), depth(level) {}
+	int y, x, dist, target;
+	Coord(int y, int x, int dist, int target) : y(y), x(x), dist(dist), target(target) {}
 };
 
-class Unordered_Hash
+bool compare(Coord* a, Coord* b)
 {
-#define PRIME 7919
-public:
-	Unordered_Hash() : curH()
-	{
-		hmap = new list<HASH*>[PRIME];
-	}
+	return a->dist > b->dist;
+}
 
-	~Unordered_Hash()
+function<bool(Coord*, Coord*)> cmp = compare;
+
+void dijkstra(int& row, int& col, int dist, int target, int& cnt, priority_queue<Coord*, vector<Coord*>, decltype(cmp)>& pq)
+{
+	if (mat[row][col] - '0' == target) return;
+
+	vector<vector<int>> dists(3, vector<int>(5, INT_MAX));
+
+	dists[row][col] = dist;
+
+	priority_queue<Coord*, vector<Coord*>, decltype(cmp)> q(cmp);
+	q.emplace(new Coord(row, col, 0, target));
+
+	while (!q.empty())
 	{
-		for (int i = 0; i < PRIME; i++)
+		auto& [r, c, d, t] = *q.top();
+		q.pop();
+
+		if (mat[r][c] - '0' == target)
 		{
-			for (HASH* p : hmap[i])
+			if (target == 4)
 			{
-				delete p;
+				cnt = min(cnt, d);
+				break;
 			}
-			hmap[i].clear();
+			pq.emplace(new Coord(r, c, d, t + 1));
 		}
-	}
 
-	uint64_t gethash(string& s)
-	{
-		uint64_t hash = 5381;
+		if (d > dists[r][c]) continue;
 
-		for (char c : s)
+		for (int i = -1; i < 2; i += 2)
 		{
-			hash = (hash << 5) + hash + c;
+			int nr = r + i, nc = c + i;
+			int newG = dists[r][c] + 1;
+
+			if (0 <= nr && nr < 3 && mat[nr][c] != '#' && dists[nr][c] > newG)
+			{
+				dists[nr][c] = newG;
+				q.emplace(new Coord(nr, c, newG, t));
+			}
+
+			if (0 <= nc && nc < 5 && mat[r][nc] != '#' && dists[r][nc] > newG)
+			{
+				dists[r][nc] = newG;
+				q.emplace(new Coord(r, nc, newG, t));
+			}
 		}
-
-		return hash;
 	}
-
-	HASH* find(string& s)
-	{
-		if (hmap[curH].empty()) return nullptr;
-
-		for (auto it = hmap[curH].begin(); it != hmap[curH].end(); it++)
-		{
-			if ((*it)->key == s) return *it;
-		}
-		return nullptr;
-	}
-
-	void insert(string& s, int depth)
-	{
-		hmap[curH].emplace_back(new HASH(s, depth));
-	}
-
-	void setHash(string& s)
-	{
-		curH = gethash(s) % PRIME;
-	}
-private:
-	list<HASH*> *hmap;
-	int curH;
-};
+}
 
 int main()
 {
-	string s = "dfewfwgwgweg";
-	Unordered_Hash hs;
-	hs.setHash(s);
-	if (!hs.find(s)) cout << "없음" << endl;
-	hs.insert(s, s.length());
-	auto p = hs.find(s);
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> mat[i];
+	}
 
-	cout << p->key << " " << p->depth << endl;
+	priority_queue<Coord*, vector<Coord*>, decltype(cmp)> q(cmp);
+	q.emplace(new Coord(0, 0, 0, 1));
+	int cnt = INT_MAX;
+	while (!q.empty())
+	{
+		auto [r, c, d, t] = *q.top();
+		q.pop();
+		dijkstra(r, c, d, t, cnt, q);
+	}
+	cout << cnt;
 	return 0;
 }
