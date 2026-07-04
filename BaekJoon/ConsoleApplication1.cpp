ad23264e39881bafb88bc2aed440837b8afc470d
vlaud <whehdgus1207@naver.com>
2026-07-04 11:53:29 +0900

    비트마스크 필터

--- BaekJoon/ConsoleApplication1.cpp
+++ BaekJoon/ConsoleApplication1.cpp
@@ -1,90 +1,202 @@
 ﻿#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-#include <functional>
-
 using namespace std;
 
-string mat[3];
-
-struct Coord
+class BitMaskFilter
 {
-	int y, x, dist, target;
-	Coord(int y, int x, int dist, int target) : y(y), x(x), dist(dist), target(target) {}
-};
-
-bool compare(Coord* a, Coord* b)
-{
-	return a->dist > b->dist;
-}
-
-function<bool(Coord*, Coord*)> cmp = compare;
-
-void dijkstra(int& row, int& col, int dist, int target, int& cnt, priority_queue<Coord*, vector<Coord*>, decltype(cmp)>& pq)
-{
-	if (mat[row][col] - '0' == target) return;
-
-	vector<vector<int>> dists(3, vector<int>(5, INT_MAX));
-
-	dists[row][col] = dist;
-
-	priority_queue<Coord*, vector<Coord*>, decltype(cmp)> q(cmp);
-	q.emplace(new Coord(row, col, 0, target));
-
-	while (!q.empty())
+public:
+	// 생성자
+	BitMaskFilter(int* inputData, int dataSize) : mData(inputData), mSize(dataSize)
 	{
-		auto& [r, c, d, t] = *q.top();
-		q.pop();
-
-		if (mat[r][c] - '0' == target)
+		mMask = 0xffffffff;
+		if (mSize < 32)
 		{
-			if (target == 4)
-			{
-				cnt = min(cnt, d);
-				break;
-			}
-			pq.emplace(new Coord(r, c, d, t + 1));
-		}
-
-		if (d > dists[r][c]) continue;
-
-		for (int i = -1; i < 2; i += 2)
-		{
-			int nr = r + i, nc = c + i;
-			int newG = dists[r][c] + 1;
-
-			if (0 <= nr && nr < 3 && mat[nr][c] != '#' && dists[nr][c] > newG)
-			{
-				dists[nr][c] = newG;
-				q.emplace(new Coord(nr, c, newG, t));
-			}
-
-			if (0 <= nc && nc < 5 && mat[r][nc] != '#' && dists[r][nc] > newG)
-			{
-				dists[r][nc] = newG;
-				q.emplace(new Coord(r, nc, newG, t));
-			}
+			mMask = (1u << mSize) - 1;
 		}
 	}
+	~BitMaskFilter() {}
+
+	void SetBit(int index, bool value)
+	{
+		if (index >= 0 && index < mSize && index < 32)
+		{
+			if (value) mMask |= (1u << index);
+			else mMask &= ~(1u << index);
+				
+		}
+	}
+
+	bool GetBit(int index)
+	{
+		if (index >= 0 && index < mSize && index < 32)
+		{
+			return (mMask & (1u << index)) != 0;
+		}
+		return false;
+	}
+
+	void SetMask(unsigned int newMask)
+	{
+		mMask = newMask;
+	}
+
+	int ApplyFilter(int* result)
+	{
+		int resultIndex = 0;
+		for (int i = 0; i < mSize && i < 32; i++)
+		{
+			if (mMask & (1u << i)) // i번째 비트 1인지 확인
+			{
+				result[resultIndex++] = mData[i];
+
+			}
+		}
+		return resultIndex;
+	}
+
+	void PrintMask()
+	{
+		cout << "Mask (binary): ";
+		for (int i = mSize - 1; i >= 0; i--)
+		{
+			cout << ((mMask & (1u << i)) ? '1' : '0');
+		}
+		cout << " (0x";
+		cout.flags(ios::hex);
+		cout << mMask;
+		cout.flags(ios::dec);
+		cout << ")" << endl;
+	}
+
+	void SetAllBits()
+	{
+		mMask = (mSize < 32) ? (1u << mSize) - 1 : 0xffffffff;
+	}
+
+	void ClearAllBits()
+	{
+		mMask = 0;
+	}
+
+	void InvertMask()
+	{
+		mMask = ~mMask;
+		if (mSize < 32)
+		{
+			mMask &= (1u << mSize) - 1;
+		}
+	}
+
+	void IntersectWith(unsigned int otherMask)
+	{
+		mMask &= otherMask;
+	}
+
+	void UnionWith(unsigned int otherMask)
+	{
+		mMask |= otherMask;
+	}
+
+	int CountSetBits()
+	{
+		int count = 0;
+		unsigned int temp = mMask;
+		while (temp)
+		{
+			count += temp & 1;
+			temp >>= 1;
+		}
+		return count;
+	}
+private:
+	int *mData, mSize;
+	unsigned int mMask;
+};
+
+void printfiltering(BitMaskFilter filter, int *result)
+{
+	filter.PrintMask();
+	int count = filter.ApplyFilter(result);
+	cout << "필터된 데이터: ";
+	for (int i = 0; i < count; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl << endl;
 }
 
 int main()
 {
-	for (int i = 0; i < 3; i++)
-	{
-		cin >> mat[i];
-	}
+	int data[] = { 10,20,30,40,50,60,70,80 };
+	int dataSize = 8;
+	int result[8];
 
-	priority_queue<Coord*, vector<Coord*>, decltype(cmp)> q(cmp);
-	q.emplace(new Coord(0, 0, 0, 1));
-	int cnt = INT_MAX;
-	while (!q.empty())
+	BitMaskFilter filter(data, dataSize);
+
+	cout << "=== 비트 마스킹 데이터 필터링 예제 ===" << endl << endl;
+
+	// 초기 상태 (모든 데이터 포함)
+	cout << "1. 초기 상태 (모든 데이터 포함):" << endl;
+	filter.PrintMask();
+	int count = filter.ApplyFilter(result);
+	cout << "필터된 데이터: ";
+	for (int i = 0; i < count; i++)
 	{
-		auto [r, c, d, t] = *q.top();
-		q.pop();
-		dijkstra(r, c, d, t, cnt, q);
+		cout << result[i] << " ";
 	}
-	cout << cnt;
+	cout << endl << endl;
+
+	cout << "2. 짝수 인덱스만 선택 (0, 2, 4, 6):" << endl;
+	filter.ClearAllBits();
+	filter.SetBit(0, true);
+	filter.SetBit(2, true);
+	filter.SetBit(4, true);
+	filter.SetBit(6, true);
+	filter.PrintMask();
+	count = filter.ApplyFilter(result);
+	cout << "필터된 데이터: ";
+	for (int i = 0; i < count; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl << endl;
+
+	cout << "3. 비트 연산으로 마스크 설정 (0b10110101):" << endl;
+	filter.SetMask(0b10110101); // 0, 2, 4, 5, 7번째
+	filter.PrintMask();
+	cout << "필터된 데이터: ";
+	count = filter.ApplyFilter(result);
+	for (int i = 0; i < count; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << "설정된 비트 개수: " << filter.CountSetBits() << endl << endl;
+
+	cout << "4.마스크 반전:" << endl;
+	filter.InvertMask();
+	filter.PrintMask();
+	count = filter.ApplyFilter(result);
+	cout << "필터된 데이터: ";
+	for (int i = 0; i < count; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl << endl;
+
+	cout << "5. 마스크 교집합 연산:" << endl;
+	filter.SetMask(0b11110000);
+	cout << "마스크 A: ";
+	filter.PrintMask();
+
+	unsigned int maskB = 0b10101010;
+	cout << "마스크 B: ";
+	for (int i = 7; i >= 0; i--)
+	{
+		cout << ((maskB & (1u << i)) ? '1' : '0');
+	}
+	cout << endl;
+
+	filter.IntersectWith(maskB);
+	cout << "교집합 결과: ";
+	printfiltering(filter, result);
 	return 0;
 }
